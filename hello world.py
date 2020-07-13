336fa330251ed85efdc253922e65921f38bb313e
AugustusMarvin <bonjouryongkai@163.com>
2020-07-13 19:54:32 +0800

    1

--- hello world.py
+++ hello world.py
@@ -3,3 +3,8 @@ a = 10
 print(a)
 list = ['marvin', 'leo', 'lea']
 print(list)
+class player():
+    def _init_(self, name, gender,age):
+        self.name = name
+        self.gender = gender
+        self.age = age
