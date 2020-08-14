5ec117cfd3d1a941a1346d4b727ed8d0e3e981bf
AugustusMarvin <bonjouryongkai@163.com>
2020-08-14 11:16:02 +0800

    8.14 11:11

--- hello world.py
+++ hello world.py
@@ -61,3 +61,80 @@ print(len(cars))
 #4.1
 for car in cars:
     print(car)
+    print(car.title() + ", that was a great car!")
+    print("I can't wait to see your new car, " + car.title() + ".\n")
+
+numbers = list(range(1, 6))
+print(numbers)
+#4.2
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+print(squares)
+sum(squares)
+#4.3
+squares = [value**2 for value in range(1, 11)]
+print(squares)
+#4.4
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[1:4])
+print(players[:3])
+print(players[-3:])
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+your_players = players[:]
+print(players)
+print(your_players)
+your_players.append('marvin')
+print(your_players)
+print(players)
+#4.5
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+for dimension in dimensions:
+    print(dimension)
+
+#5.4
+requested_toppings = ['mushrooms', 'green peppers', 'french fries']
+#requested_toppings = []
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+'prpperono', 'pineapple', 'extra cheese']
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            if requested_topping == 'green peppers':
+                print("Sorry, we are out of green peppers right now.")
+            else:
+                print("Adding " + requested_topping + ".")
+        else:
+            print("Sorry, we don't have " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+#No.6
+#6.1
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+alien_0['color'] = 'yellow'
+print(alien_0)
+#6.2
+alien_0['speed'] = 'medium'
+print("The original x-position: " + str(alien_0['x_position']))
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position'])) 
