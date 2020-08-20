1b3acd2dd4e7be763b924ccbdf3fde54595a54db
AugustusMarvin <bonjouryongkai@163.com>
2020-08-20 21:43:15 +0800

    2020.8.20

--- hello world.py
+++ hello world.py
@@ -238,3 +238,91 @@ for username, user_info in users.items():
     location = user_info['location']
     print("\tFullname is " + full_name.title())
     print("\tLocation is " + location.title())
+#No.7
+#7.1
+'''
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print("\nHello, " + name + "!")
+'''
+'''
+age = input("How old are you? ")
+age = int(age)
+if age >= 18:
+    print("\nYou're a adult now! ")
+else:
+    print("\nYou're still a child! ")
+'''
+'''
+number = input("\nEnter a number, and I will tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
+'''
+#7.2
+'''
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter ' quit ' to end the program. "
+active = True
+while active:
+    message = input(prompt)
+    
+    if message != 'quit':
+        print(message)
+    else:
+        active = False
+'''
+'''
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter ' quit ' to end the program. "
+while True:
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+    else:
+        break
+'''
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
+#7.3
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user)
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
+
+responses = {}
+polling_active = True
+while polling_active:
+    name = input("\nPlease enter your name: ")
+    response = input("\nWhich mountain would you like to climb someday?")
+    responses[name] = response
+    repeat = input("Would you like to let another person response?(yes/no) ")
+    if repeat == "no":
+        polling_active = False
+print("\n-- Poll Results --")
+for name, response in responses.items():
+    print(name.title() + " would like to climb " + response + ".")
