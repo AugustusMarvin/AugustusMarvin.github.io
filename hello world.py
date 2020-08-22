d8bdc336c15a3d7f1b501d70136f5a5128977161
AugustusMarvin <bonjouryongkai@163.com>
2020-08-22 22:01:39 +0800

    2020.8.22更新第九章学习笔记，新增说明文件

--- hello world.py
+++ hello world.py
@@ -456,3 +456,91 @@ user_profile = build_profile('marvin', 'wang',
                              location='princeton',
                              field='physics')
 print(user_profile)
+
+#No.9
+#9.1
+class Dog():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def sit(self):
+        print(self.name.title() + " is now sitting!")
+    def roll_over():
+        print(self.name.title() + " rolled over!")
+my_dog = Dog('bvb', 6)
+print("My dog's name is " + my_dog.name.title() + ".")
+print("The age of my dog is " + str(my_dog.age) + " year's old.")
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+my_new_car = Car('audi', 'a4', 2020)
+print("My new car is " + my_new_car.get_descriptive_name())
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+my_new_car = Car('audi', 'a4', 2020)
+my_new_car.odometer_reading = 23
+print("My new car is " + my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!!")
+my_new_car = Car('audi', 'a4', 2020)
+print("My new car is " + my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.update_odometer(20)
+my_new_car.read_odometer()
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+my_new_car = Car('audi', 'a4', 2020)
+print("My new car is " + my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23000)
+my_new_car.update_odometer(20)
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
