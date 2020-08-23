9ab40190e9b061e5e5b2b13ee6556439163decaa
AugustusMarvin <bonjouryongkai@163.com>
2020-08-23 20:31:28 +0800

    2020.8.23 第九章笔记完成

--- hello world.py
+++ hello world.py
@@ -465,12 +465,12 @@ class Dog():
         self.age = age
     def sit(self):
         print(self.name.title() + " is now sitting!")
-    def roll_over():
+    def roll_over(self):
         print(self.name.title() + " rolled over!")
 my_dog = Dog('bvb', 6)
 print("My dog's name is " + my_dog.name.title() + ".")
 print("The age of my dog is " + str(my_dog.age) + " year's old.")
-
+'''
 class Car():
     def __init__(self, mark, model, year):
         self.mark = mark
@@ -544,3 +544,100 @@ my_new_car.update_odometer(23000)
 my_new_car.update_odometer(20)
 my_new_car.increment_odometer(100)
 my_new_car.read_odometer()
+'''
+#9.3
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+    def fill_gas_tank(self):
+        print("The gastank of my car is full!!")
+    
+class ElectricCar_1(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+    def fill_gas_tank(self):
+        print("This car doesn't need a gas tank!!")
+my_tesla = ElectricCar_1('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
+
+class Car():
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.mark + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class Battery():
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+
+class ElectricCar_1(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar_1('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
