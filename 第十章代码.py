0950007b56281b7f050e2ed55b35c8e9d2114afb
AugustusMarvin <bonjouryongkai@163.com>
2020-09-12 10:45:45 +0800

    第十章学习完成

--- 第十章代码.py
+++ 第十章代码.py
@@ -71,10 +71,53 @@ else:
     print(contents)
 #10.3存储数据
 import json
+'''
 numbers = [2, 3, 5, 7, 11, 13]
 filename = 'E:/python_wenjian/numbers.json'
+#创建json文件时，斜杆是/
 with open(filename, 'w') as f_obj:
     json.dump(numbers, f_obj)
 with open(filename) as f_obj:
     numbers = json.load(f_obj)
 print(numbers)
+username = input("What is your nanme? ")
+filename = 'E:/python_wenjian/username.json'
+with open(filename, "w") as f_obj:
+    json.dump(username, f_obj)
+    print("We'll remember you when you come back, " + username + "!\n")
+with open(filename) as f_obj:
+    username = json.load(f_obj)
+    print("Welcome back, " + username + "!")
+'''
+'''
+filename = 'E:/python_wenjian/username_1.json'
+try:
+    with open(filename, "r") as f_obj:
+        username = json.load(f_obj)
+except FileNotFoundError:
+    username = input("What's your name?")
+    with open(filename, "w") as f_obj:
+        json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!\n")
+else:
+    print("Welcome back, " + username + "!")
+'''
+def get_stored_username():
+    filename = 'E:/python_wenjian/username_1.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = input("What's your name?")
+        with open(filename, "w") as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + username + "!\n")
+greet_user()
